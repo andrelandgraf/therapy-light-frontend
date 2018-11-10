ba5a1d761d59ac660444f2019a4c9c96e8fd3b48
Andre Landgraf <andre.timo.landgraf@gmail.com>
2018-11-10 17:38:58 +0100

    added video canvas and webrtc stream

--- src/App.js
+++ src/App.js
@@ -5,16 +5,41 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.stream = undefined;
+    this.state = {
+      stream: undefined,
+    }
   }
+
+  componentDidMount() {
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+              video: true,
+              audio: true
+          })
+          .then(this.setStream)
+          .catch(function (error) {
+              console.log("Something went wrong!");
+              console.log(error.message);
+          });
+    }
+  }
+
+  setStream = (stream) => {
+    console.log(stream);
+    // for debugging make variable available to browser console:
+    // window.stream = stream;
+    this.setState({stream: stream});
+  }
+
   render() {
+    const { stream } = this.state;
     return (
       <div className="App">
         <header className="App-header">
             <h1>Therapylight - Your Chance to Hack your Relationship</h1>
             <h2>Creative Hack 2018 Netlight & Microsoft Student Partners</h2>
         </header>
-        <VideoCanvas/ >
+        <VideoCanvas stream={stream}/ >
       </div>
     );
   }
--- src/components/VideoCanvas.js
+++ src/components/VideoCanvas.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import '../styles/VideoCanvas.css';
 
-const VideoCanvas = ({ stream }) => (
-    <div>Video Canvas</div>
-);
+class VideoCanvas extends React.Component { 
+
+    componentDidUpdate(){
+        const { stream } = this.props;
+        console.log(stream);
+        const video = document.querySelector("#video-element");
+        if(stream !== undefined){
+            video.srcObject = stream;
+        }
+    }
+    
+    render(){
+        return (
+            <div id="video-container">
+                <video autoPlay={true} id="video-element"></video>
+            </div>
+        );
+    }
+}
 
 export default VideoCanvas;
